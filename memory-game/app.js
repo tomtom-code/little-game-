a8f3a8b46c05651c2b135edbeaa60a43f0803229
tomtom-code <tomlai8882000@hotmail.com>
2021-03-11 19:12:49 +0800

    add image and more html

---
+++ memory-game/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        }
+    ]
+})
