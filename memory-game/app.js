ac867ed597e72bb3a34d30ede0715e55ab58c2a7
tomtom-code <tomlai8882000@hotmail.com>
2021-03-11 22:04:51 +0800

    finish set up the card

--- memory-game/app.js
+++ memory-game/app.js
@@ -9,6 +9,69 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: 'cheeseburger',
             img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'   
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
         }
+        
+      
+
     ]
+
+    const grid = document.querySelector('.grid')
+
+    //create your board
+    function createBoard() {
+        for (i = 0; i<cardArray.length; i++){
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+
+            // card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard()
+
+
+
+    
 })
