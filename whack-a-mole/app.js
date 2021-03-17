bc6b8582279898b8d678eb0da0d0c1c2d0197aee
tomtom-code <tomlai8882000@hotmail.com>
2021-03-17 19:39:00 +0800

    app.js

---
+++ whack-a-mole/app.js
@@ -0,0 +1,6 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
