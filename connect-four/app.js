ceefaf8e6f03d7fc1a8b60750d2be14368472a3a
tomtom-code <tomlai8882000@hotmail.com>
2021-03-25 20:50:36 +0800

    app.js

---
+++ connect-four/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
