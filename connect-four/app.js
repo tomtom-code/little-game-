e19495d7ff91c43345723b15d1b0644cd3ea31bf
tomtom-code <tomlai8882000@hotmail.com>
2021-03-26 12:09:26 +0800

    app.js

--- connect-four/app.js
+++ connect-four/app.js
@@ -3,3 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#current-player')
     let currentPlayer = 1
+
+    for (let i = 0, len = squares.length; i < len; i++)
+
+    (function(index){
+    //add an onclick to each square in your grid
+        squares[i].onclick = function(){
+            //if the square below your current square is taken, you can go ontop of it
+            if(squares[index + 7].classList.contains('taken')){
+                if (currentPlayer === 1) {
+                    squares[index].classList.add('taken')
+                    squares[index].classList.add('player-one')
+                    //change the player
+                    currentPlayer = 2
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                } else if (currentPlayer === 2) {
+                    squares[index].classList.add('taken')
+                    squares[index].classList.add('player-two')
+                    //change the player
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+                //if the square below your current square is not taken, you can't go there
+              } else alert('cant go here')
+          }
+    })(i)
