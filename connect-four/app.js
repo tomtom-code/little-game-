91a23053f4a6ed013d35dee8466bb07ac6d7a120
tomtom-code <tomlai8882000@hotmail.com>
2021-03-31 17:31:02 +0800

    app.js

--- connect-four/app.js
+++ connect-four/app.js
@@ -76,6 +76,21 @@ document.addEventListener('DOMContentLoaded', () => {
         [13, 20, 27, 34],
       ]
 
+      function checkBoard() {
+        for (let y = 0; y < winningArrays.length; y++) {
+          const square1 = squares[winningArrays[y][0]]
+          const square2 = squares[winningArrays[y][1]]
+          const square3 = squares[winningArrays[y][2]]
+          const square4 = squares[winningArrays[y][3]]
+    
+          //check those squares to see if they all have the class of player-one
+          if (
+            square1.classList.contains('player-one') &&
+            square2.classList.contains('player-one') &&
+            square3.classList.contains('player-one') &&
+            square4.classList.contains('player-one')
+          )
+
     for (let i = 0, len = squares.length; i < len; i++)
 
     (function(index){
