a78ab6f984144616e66c7815aa1ab8036d30eee0
tomtom-code <tomlai8882000@hotmail.com>
2021-04-15 18:50:00 +0800

    app update

--- Nokia-Snake/app.js
+++ Nokia-Snake/app.js
@@ -70,6 +70,21 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[appleIndex].classList.add('apple')
   }
 
+  function control(e) {
+    squares[currentIndex].classList.remove('snake')
+
+    if(e.keyCode === 39) {
+      direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
+    } else if (e.keyCode === 38) {
+      direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
+    } else if (e.keyCode === 37) {
+      direction = -1 // if we press left, the snake will go left one div
+    } else if (e.keyCode === 40) {
+      direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+    }
+  }
+
+
 
 
 })
