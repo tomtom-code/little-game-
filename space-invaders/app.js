737425af2de26a80a6d8218f5e43a30f3a458c3c
tomtom-code <tomlai8882000@hotmail.com>
2021-04-20 22:25:55 +0800

    app

---
+++ space-invaders/app.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+let aliensRemoved = []
+let results = 0
