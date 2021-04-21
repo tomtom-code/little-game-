501b44b4a614c696e5a278c2fc38ef8ab5b8ddd5
tomtom-code <tomlai8882000@hotmail.com>
2021-04-21 18:41:48 +0800

    app

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -7,3 +7,16 @@ let invadersId
 let goingRight = true
 let aliensRemoved = []
 let results = 0
+
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+  }
+  
+  const squares = Array.from(document.querySelectorAll('.grid div'))
+  
+  const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+  ]
