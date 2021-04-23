be7fd0cd5590f14ccf9d43c068ceca2c43ec1e42
tomtom-code <tomlai8882000@hotmail.com>
2021-04-23 18:29:00 +0800

    app adding shooting part

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -36,4 +36,19 @@ function remove() {
       squares[alienInvaders[i]].classList.remove('invader')
     }
 }
-  
+
+squares[currentShooterIndex].classList.add('shooter')
+
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove('shooter')
+  switch(e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+      break
+    case 'ArrowRight' :
+      if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+      break
+  }
+  squares[currentShooterIndex].classList.add('shooter')
+}
