8baf8d2cea8380d940a53231a7335f218af2afd4
tomtom-code <tomlai8882000@hotmail.com>
2021-05-03 23:43:45 +0800

    update apps part

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -95,3 +95,29 @@ function moveInvaders() {
   }
 }
 
+function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+    function moveLaser() {
+      squares[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      squares[currentLaserIndex].classList.add('laser')
+  
+      if (squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
+  
+        setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+        clearInterval(laserId)
+  
+        const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+        aliensRemoved.push(alienRemoved)
+        results++
+        resultsDisplay.innerHTML = results
+        console.log(aliensRemoved)
+  
+      }
+  
+    }
+
