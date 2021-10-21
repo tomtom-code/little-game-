b79767a2bc6b0c7e8fc00f957bad0acc863ba8b7
tomtom-code <tomlai8882000@hotmail.com>
2021-10-20 23:47:07 -0500

    add in Frogger js

--- Frogger/app.js
+++ Frogger/app.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const squares = document.querySelectorAll('.grid div')
+    const timeLeft = document.querySelector('#time-left')
+    const result = document.querySelector('#result')
+    const startBtn = document.querySelector('#button')
 })
