05d7d16435332975eb3d0100193f6ee34c71f512
tomtom-code <tomlai8882000@hotmail.com>
2021-10-21 23:27:55 -0500

    add on css and js for Frog

--- Frogger/app.js
+++ Frogger/app.js
@@ -25,7 +25,14 @@ document.addEventListener('DOMContentLoaded', () => {
             case 39:
                 if(currentIndex % width < width -1) currentIndex +=1
                 break
+            case 40:
+                if(currentIndex + width < width * width) currentIndex += width
+                break
         }
+
+        squares[currentIndex].classList.add('frog')
+        lose()
+        win()
     }
 
 
