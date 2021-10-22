cbfdd6cdcac0e13c9ebfcbfcb659c2090aafe4f3
tomtom-code <tomlai8882000@hotmail.com>
2021-10-21 22:47:08 -0500

    add moving function in JS for Frog

--- Frogger/app.js
+++ Frogger/app.js
@@ -6,11 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#button')
     const width = 9
     let currentIndex = 76
+    let timerId
 
     // render frog on starting block
 
-    squares[currentIndex],classList.add('frog')
+    squares[currentIndex].classList.add('frog')
 
+    // write a function that will move the Frog
+    function movceFrog(e){
+        squares[currentIndex].classList.remove('frog')
+        switch(e.keyCode){
+            case 37:
+                if(currentIndex % width !==0) currentIndex -=1
+                break
+            case 38:
+                if(currentIndex - width >=0) currentIndex -= width
+                break
+            case 39:
+                if(currentIndex % width < width -1) currentIndex +=1
+                break
+        }
+    }
 
 
 })
