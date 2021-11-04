8008d697720caad0d319fbb16be1d5764cb26f65
tomtom-code <tomlai8882000@hotmail.com>
2021-11-03 23:57:25 -0500

    set some Tetromino

--- Tetris/app.js
+++ Tetris/app.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const grid = document.querySelector('.grid')
     const width = 10
     const height = 20
 
-    
+ // the tetrominoes
+ const l Tetromino = [
+     [1, width+1, width*2+1, 2],
+     [width, width+1, width+2, width*2+2],
+     [1, width+1, width*2+1, width*2],
+     [width, width*2, width*2+1, width*2+1]
+
+ ]
 
 }
 
