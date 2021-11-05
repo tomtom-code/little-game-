bce36b40daf0b6fbf2c7cc3c48f6f776460cdf2c
tomtom-code <tomlai8882000@hotmail.com>
2021-11-05 16:30:29 -0500

    add tetromino

--- Tetris/app.js
+++ Tetris/app.js
@@ -4,12 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const height = 20
 
  // the tetrominoes
- const l Tetromino = [
+ const lTetromino = [
      [1, width+1, width*2+1, 2],
      [width, width+1, width+2, width*2+2],
      [1, width+1, width*2+1, width*2],
      [width, width*2, width*2+1, width*2+1]
 
+ ]
+
+ const zTetromino = [
+     [0, width, width+1, width*2+1],
+     [width+1, width+2, width*2, width*2+1],
+     [0, width, width+1, width*2+1],
+     [width+1, width+2, width*2, width*2+1]
+ ]
+
+ const tTetromino = [
+     []
  ]
 
 }
